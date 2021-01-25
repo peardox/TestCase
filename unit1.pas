4c0494c9a0a1040e57b5005f081c8577fce81c3f
Simon Booth <simon@peardox.com>
2021-01-25 19:07:54 +0000

    A testcase

--- unit1.pas
+++ unit1.pas
@@ -17,11 +17,14 @@ type
     CastleControlBase2: TCastleControlBase;
     Splitter1: TSplitter;
     procedure CastleControlBase1Open(Sender: TObject);
+    procedure CastleControlBase2Open(Sender: TObject);
   private
 
   public
 
   end;
+
+  { TCastleApp }
 
   TCastleApp = class(TUIState)
     procedure Resize; override; // TCastleUserInterface
@@ -32,15 +35,41 @@ type
     procedure LoadView;
   end;
 
+  { TCardPreview }
+
+  TCardPreview = class(TCastleUserInterface)
+    procedure Resize; override;
+  private
+    PreviewImage: TCastleImageControl;
+  public
+    procedure Start;
+    procedure LoadView;
+    procedure LoadPreview(filename: String);
+  end;
+
 var
   Form1: TForm1;
   CastleApp: TCastleApp;
+  CardPreview: TCardPreview;
+
+const
+  DefaultImage: String = 'castle-data:/badImage.png';
+  DefaultPreviewImage: String = 'castle-data:/previewImage.png';
 
 implementation
 
 {$R *.lfm}
 
 { TForm1 }
+
+procedure TForm1.CastleControlBase2Open(Sender: TObject);
+begin
+  CardPreview := TCardPreview.Create(Application);
+  CardPreview.FullSize := True;
+  CastleControlBase2.Controls.InsertFront(CardPreview);
+//  CardPreview.Start;
+  CastleControlBase2.Container.UIScaling := usDpiScale;
+end;
 
 procedure TForm1.CastleControlBase1Open(Sender: TObject);
 begin
@@ -58,7 +87,7 @@ begin
   AnImage.Stretch := True;
   AnImage.ProportionalScaling := psFit;
   InsertFront(AnImage);
-  AnImage.URL := 'castle-data:/previewImage.png';
+  AnImage.URL := DefaultImage;
 end;
 
 procedure TCastleApp.Start;
@@ -74,6 +103,45 @@ begin
   AnImage.Height := StateContainer.Height;
   AnImage.Left := (AnImage.Width - AnImage.EffectiveWidth) / 2;
   AnImage.Bottom := (AnImage.Height - AnImage.EffectiveHeight) / 2;
+end;
+
+procedure TCardPreview.LoadView;
+begin
+  PreviewImage := TCastleImageControl.Create(Application);
+  PreviewImage.Width := Width;
+  PreviewImage.Height := Height;
+  PreviewImage.Stretch := True;
+  PreviewImage.ProportionalScaling := psFit;
+  InsertFront(PreviewImage);
+end;
+
+procedure TCardPreview.LoadPreview(filename: String);
+begin
+  try
+    PreviewImage.URL := filename;
+  except
+    on E : Exception do
+      begin
+        ShowMessage('TCardPreview.LoadFrame' + LineEnding + E.ClassName + LineEnding + E.Message);
+       end;
+  end;
+end;
+
+procedure TCardPreview.Start;
+begin
+  inherited;
+  PreviewImage := nil;
+  LoadView;
+  LoadPreview(DefaultPreviewImage);
+end;
+
+procedure TCardPreview.Resize;
+begin
+  inherited;
+  PreviewImage.Width := Width;
+  PreviewImage.Height := Height;
+  PreviewImage.Left := (PreviewImage.Width - PreviewImage.EffectiveWidth) / 2;
+  PreviewImage.Bottom := (PreviewImage.Height - PreviewImage.EffectiveHeight) / 2;
 end;
 
 
